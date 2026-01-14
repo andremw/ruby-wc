1e62a353320f000ec45ea22354fd68c9f0ea2c1c
andremw <mazoni.andre@gmail.com>
2026-01-14 19:32:38 +1030

    feat: enabling watch mode and colors for tests

--- Gemfile
+++ Gemfile
@@ -9,5 +9,11 @@ gem "irb"
 gem "rake", "~> 13.0"
 
 gem "minitest", "~> 5.16"
+gem 'minitest-reporters'
 
 gem "standard", "~> 1.3"
+
+group :development do
+  gem 'guard'
+  gem 'guard-minitest'
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+require "minitest/reporters"
+Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new(color: true)
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "ruby/wc"
