b6208548142a2d89dc8f912cf1192f2c32600d0d
andremw <mazoni.andre@gmail.com>
2026-01-14 20:48:29 +1030

    test: listing test cases for integration_test

---
+++ test/ruby/test_wc.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Ruby::TestInputParser < Minitest::Test
+  def test_it_counts_bytes_in_a_file
+    skip "todo"
+  end
+
+  def test_it_counts_lines_in_a_file
+    skip "todo"
+  end
+
+  def test_it_counts_words_in_a_file
+    skip "todo"
+  end
+
+  def test_it_counts_characters_in_a_file
+    skip "todo"
+  end
+
+  def test_it_counts_bytes_lines_and_words_in_a_file_if_no_flag_is_passed
+    skip "todo"
+  end
+end
