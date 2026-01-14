fee96e4a051b4f5c02051bb6454f088fef638bf4
andremw <mazoni.andre@gmail.com>
2026-01-14 20:21:08 +1030

    test: defining initial test cases

---
+++ test/ruby/test_input_parser.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "test_helper"
+require "ruby/wc/input_parser"
+
+class Ruby::TestInputParser < Minitest::Test
+  def test_it_parses_the_c_flag
+    skip "todo"
+  end
+
+  def test_it_parses_the_l_flag
+    skip "todo"
+  end
+
+  def test_it_parses_the_w_flag
+    skip "todo"
+  end
+
+  def test_it_parses_the_m_flag
+    skip "todo"
+  end
+end
